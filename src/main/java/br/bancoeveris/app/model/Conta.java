5898b181249d3081bf3cc9374c97d8ac21a954d7
Francieli Silva Abreu <fsilvaab@everis.com>
2020-11-06 16:22:27 -0300

    atualização de banco de dados

--- src/main/java/br/bancoeveris/app/model/Conta.java
+++ src/main/java/br/bancoeveris/app/model/Conta.java
@@ -13,35 +13,36 @@ public class Conta {
 	private Long Id;
 	private String Hash;
 	private String Nome;
-	private String Cpf;
 	private double Saldo = 0;
+
 	public Long getId() {
 		return Id;
 	}
+
 	public void setId(Long id) {
 		Id = id;
 	}
+
 	public String getHash() {
 		return Hash;
 	}
+
 	public void setHash(String hash) {
 		Hash = hash;
 	}
+
 	public String getNome() {
 		return Nome;
 	}
+
 	public void setNome(String nome) {
 		Nome = nome;
 	}
-	public String getCpf() {
-		return Cpf;
-	}
-	public void setCpf(String cpf) {
-		Cpf = cpf;
-	}
+
 	public double getSaldo() {
 		return Saldo;
 	}
+
 	public void setSaldo(double saldo) {
 		Saldo = saldo;
 	}
